4eaddea317216f152dd76f24ef1df8a41dd298cd
Leonardo C. Almeida <leocalm@gmail.com>
2019-10-22 23:41:58 +0200

    Changing docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,11 +49,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-pygments_style = 'sphinx'
-
-html_theme = 'neo_rtd_theme'
-import sphinx_theme
-html_theme_path = [sphinx_theme.get_html_theme_path()]
+# pygments_style = 'sphinx'
+#
+# html_theme = 'neo_rtd_theme'
+# import sphinx_theme
+# html_theme_path = [sphinx_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
